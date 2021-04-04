e00b26e642afd22472dce17bd96c1b6850ab6276
Al-AminJoy <alaminislam3555@gmail.com>
2021-04-04 17:59:14 +0600

    Bug Fixed

--- todolist/lib/src/App.dart
+++ todolist/lib/src/App.dart
@@ -4,31 +4,10 @@ import 'package:todolist/screen/home_screen.dart';
 class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    /*return new MaterialApp(
+    return new MaterialApp(
       debugShowCheckedModeBanner: false,
       home: new HomeScreen(),
-    );*/
-    return FutureBuilder(
-      // Replace the 3 second delay with your initialization code:
-      future: Future.delayed(Duration(seconds: 3),),
-      builder: (context, AsyncSnapshot snapshot) {
-        // Show splash screen while waiting for app resources to load:
-        return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            home: HomeScreen());
-      } );
+    );
+
   }
 }
-  class Splash extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-  return Scaffold(
-  body: Center(
-  child: Icon(
-  Icons.apartment_outlined,
-  size: MediaQuery.of(context).size.width * 0.785,
-  ),
-  ),
-  );
-  }
-  }
